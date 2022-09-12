92aed4e2c0b002a9b09e2e8a9c2f117fe59ae06f
Emre Can KARA <afsn_emre@hotmail.com>
2022-09-12 21:30:39 +0300

    todo add message and status changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React,{ useState } from "react";
 
 function App() {
 
-  const [todoText, setTodoText] = useState("")
+  const [todoText, setTodoText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+
+  const changeIsDone = (id) => {
+
+      const searchedTodo = todos.find((item) => item.id === id);
+
+      const updatedTodo = {
+          ...searchedTodo,
+          isDone: !searchedTodo.isDone,
+      };
+
+
+      const filteredTodos = todos.filter((item) => item.id !== id);
+
+      setTodos([updatedTodo, ...filteredTodos]);
+      console.log(setTodos);
+
+  };
+
 
   const handleSubmit = (event) => {
       event.preventDefault();
@@ -12,8 +32,26 @@ function App() {
           return;
       }
 
-      alert(todoText);
+      const hasTodos = todos.find(item => item.text === todoText);
+
+      if (hasTodos !== undefined)
+      {
+          alert("You have the todo already");
+          return;
+      }
+
+      const newTodo={
+          id: new Date().getTime(),
+          isDone: false,
+          text: todoText,
+          date: new Date(),
+      }
+    
+      setTodos([newTodo, ...todos]);
+      setTodoText("");
   };
+
+
 
   return (
     <div className="container">
@@ -25,6 +63,30 @@ function App() {
             <button className="btn btn-outline-secondary" type="submit">ADD</button>
         </div>
       </form>
+
+      {
+          todos.length <= 0 ? (
+              <p className="text-center my-5">You don't have any todos yet.</p>
+          ) : (
+
+            <div>
+               {
+                  todos.map((item) => (
+                      <div className="alert alert-primary d-flex justify-content-between align-items-center">
+                          <p>{item.text}</p>
+                          <button onClick={() => changeIsDone(item.id) } className="btn btn-primary">
+                              {
+                                 item.isDone === false ? "Done" : "Undone"
+                              }
+                          </button>
+                      </div>
+                    )
+                  )
+               }
+            </div>
+
+          )
+      }
     </div>
   );
 }
