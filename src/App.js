7b38aa9c19997d4eba84096db431802498fa590f
Emre Can KARA <afsn_emre@hotmail.com>
2022-09-21 21:31:01 +0300

    changes todo react 2 videos

--- src/App.js
+++ src/App.js
@@ -1,92 +1,86 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-
   const [todoText, setTodoText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
 
+  const editTodo = (id) => {
+    setIsEdit(true);
+    const searchedTodo = todos.find((item) => item.id === id);
+    setTodoText(searchedTodo.text);
+  }
 
   const changeIsDone = (id) => {
-
-      const searchedTodo = todos.find((item) => item.id === id);
-
-      const updatedTodo = {
-          ...searchedTodo,
-          isDone: !searchedTodo.isDone,
-      };
-
-
-      const filteredTodos = todos.filter((item) => item.id !== id);
-
-      setTodos([updatedTodo, ...filteredTodos]);
-      console.log(setTodos);
-
+    const searchedTodo = todos.find((item) => item.id === id);
+    const updatedTodo = {
+      ...searchedTodo,
+      isDone: !searchedTodo.isDone,
+    };
+    const filteredTodos = todos.filter((item) => item.id !== id);
+    setTodos([updatedTodo, ...filteredTodos]);
   };
-
 
   const handleSubmit = (event) => {
-      event.preventDefault();
-      if (todoText === "")
-      {
-          alert("Todo text can't be empty !!!");
-          return;
-      }
+    event.preventDefault();
+    if (todoText === "") {
+      alert("Todo text can't be empty!");
+      return;
+    }
+    const hasTodos = todos.find((item) => item.text === todoText);
+    console.log(hasTodos);
+    if (hasTodos !== undefined) {
+      alert("You have the todo already");
+      return;
+    }
+    const newTodo = {
+      id: new Date().getTime(),
+      isDone: false,
+      text: todoText,
+      date: new Date(),
+    };
 
-      const hasTodos = todos.find(item => item.text === todoText);
-
-      if (hasTodos !== undefined)
-      {
-          alert("You have the todo already");
-          return;
-      }
-
-      const newTodo={
-          id: new Date().getTime(),
-          isDone: false,
-          text: todoText,
-          date: new Date(),
-      }
-    
-      setTodos([newTodo, ...todos]);
-      setTodoText("");
+    setTodos([...todos, newTodo]);
+    setTodoText("");
   };
-
-
-
   return (
     <div className="container">
-      <h1 className="text-center my-5">To Do App Project</h1>
-
-      <form onSubmit={ handleSubmit }>
+      <h1 className="text-center my-5">Todo App</h1>
+      <form onSubmit={handleSubmit}>
         <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Type your todo.. ." value={todoText} onChange={(event)=> setTodoText(event.target.value) } />
-            <button className="btn btn-outline-secondary" type="submit">ADD</button>
+          <input
+            value={todoText}
+            type="text"
+            className="form-control"
+            placeholder="Type your todo"
+            onChange={(event) => setTodoText(event.target.value)}
+          />
+          <button className="btn btn-primary" type="submit">
+            ADD
+          </button>
         </div>
       </form>
+      {todos.length <= 0 ? (
+        <p className="text-center my-5">You don't have any todos yet.</p>
+      ) : (
+        <>
+          {todos.map((item) => (
+            <div className={`alert alert-${item.isDone === false ? "secondary" : "success"} d-flex justify-content-between align-items-center`}>
+              <p>{item.text}</p>
+              
+              <div>
+                <button className="btn btn-sm btn-success mx-2" onClick={() => editTodo(item.id)}>Edit</button>
+                <button onClick={() => changeIsDone(item.id)} className="btn btn-sm btn-secondary">
+                  {item.isDone === false ? "Done" : "Undone"}
+                </button>
+              </div>
 
-      {
-          todos.length <= 0 ? (
-              <p className="text-center my-5">You don't have any todos yet.</p>
-          ) : (
 
-            <div>
-               {
-                  todos.map((item) => (
-                      <div className="alert alert-primary d-flex justify-content-between align-items-center">
-                          <p>{item.text}</p>
-                          <button onClick={() => changeIsDone(item.id) } className="btn btn-primary">
-                              {
-                                 item.isDone === false ? "Done" : "Undone"
-                              }
-                          </button>
-                      </div>
-                    )
-                  )
-               }
+
             </div>
-
-          )
-      }
+          ))}
+        </>
+      )}
     </div>
   );
 }
